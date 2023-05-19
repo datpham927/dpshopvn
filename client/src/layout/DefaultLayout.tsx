1e42136aa72665228e1779f35340898d48b77d1d
unknown <datp1907@gmail.com>
2023-05-19 20:13:06 +0700

    fix width

--- client/src/layout/DefaultLayout.tsx
+++ client/src/layout/DefaultLayout.tsx
@@ -8,9 +8,11 @@ interface DefaultLayoutProps {
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
     const { openSearchResults } = useAppSelector((state) => state.action);
     return (
-        <div className="flex flex-col max-w-7xl mx-auto">
+        <div className="flex flex-col  mx-auto">
             <Header />
+            <div className='flex flex-col mx-auto max-w-7xl'>
             {children}
+            </div>
             {openSearchResults && (
                 <div id="overlay" className="fixed w-screen h-screen right-0 top-0 bg-overlay z-[900]"></div>
             )}
