548b1596d6e6271f182ce322ae5b07817e0dd716
unknown <datp1907@gmail.com>
2023-05-19 20:27:05 +0700

    api/delete history

--- server/src/routes/searchRoutes.js
+++ server/src/routes/searchRoutes.js
@@ -6,5 +6,6 @@ const { verifyAccessToken } = require("../middlewares/verifyToken")
 router.post("/add", [verifyAccessToken], searchController.addHistory)
 router.get("/all_histories", [verifyAccessToken], searchController.searchHistory)
 router.get("/suggest", searchController.suggestResult)
+router.delete("/:tid/update", searchController.deleteHistory)
 
 module.exports = router
