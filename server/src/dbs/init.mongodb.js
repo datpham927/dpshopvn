7444043c5daef7193831498c7943489539f7f1fa
unknown <datp1907@gmail.com>
2023-10-09 10:58:05 +0700

    add file init.mongodb

---
+++ server/src/dbs/init.mongodb.js
@@ -0,0 +1,28 @@
+"use strict"
+const mongoose = require("mongoose")
+
+
+class Database {
+    constructor() {
+        this.connect()
+    }
+    connect(type="mongodb") {
+        if (1 === 1) {
+            mongoose.set("debug", true)
+            mongoose.set("debug", { color: true })
+        }
+        mongoose.connect(process.env.MONGODB_URL)
+        .then(() => console.log("connected successfully!"))
+        .catch(() => console.log("connection failed!"))
+    }
+    //   only init 1 connect
+    static getInstance() {
+        if (!Database.instance) {
+            Database.instance = new Database()
+        }
+        return Database.instance
+    }
+}
+
+const instanceMongoDb = Database.getInstance()
+module.exports = instanceMongoDb
--- server/src/server.js
+++ server/src/server.js
@@ -22,13 +22,11 @@ app.use(helmet()) // bảo vệ ứng dụng khỏi các cuộc tấn công bả
 app.use(bodyParser.json());//để có thể truyền được chuỗi json
 //để phân tích và trích xuất dữ liệu từ phần thân (body) của các yêu cầu HTTP có định dạng "x-www-form-urlencoded". Đây là một trong những loại dữ liệu phổ biến được sử dụng khi gửi dữ liệu từ một trang web HTML thông qua form.
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }))
-// router 
+// init router 
 routes(app)
-mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("connected successfully!"))
-    .catch(() => console.log("connection failed!"))
 
-
+// init database
+require("./dbs/init.mongodb")
 //------------ socket ---------------
 const Server = http.createServer(app)
 createSocket(Server)
